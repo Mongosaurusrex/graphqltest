b33e0827a897947d4c5e5d77d42a9d35a051c3b1
Nathan Dygant <nathan.dygant@paradoxinteractive.com>
2020-08-17 16:08:23 +0200

    Adding schema for mutation of adding a new job

---
+++ client/src/requests.js
@@ -0,0 +1,85 @@
+const endpointURL = "http://localhost:9000/graphql";
+
+async function graphqlRequest(query, variables = {}) {
+  const response = await fetch(endpointURL, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  const { data, errors } = await response.json();
+
+  if (errors) {
+    const message = errors.map((error) => error.message).join("\n");
+    throw new Error(message);
+  }
+  return data;
+}
+
+export async function loadJobs() {
+  const query = `{
+    jobs {
+      id
+      title 
+      company {
+        id
+        name
+      }
+    }
+  }`;
+
+  const { jobs } = await graphqlRequest(query);
+  return jobs;
+}
+
+export async function loadJob(id) {
+  const query = `query JobQuery($id: ID!){
+        job(id: $id) {
+          id
+          title
+          company {
+            id
+            name
+          }
+          description
+        }
+      }`;
+
+  const { job } = await graphqlRequest(query, { id });
+  return job;
+}
+
+export async function loadCompany(id) {
+  const query = `query CompanyQuery($id: ID!){
+        company(id: $id) {
+       id
+       name
+       description
+       jobs {
+        id
+        title
+      }
+     }
+   }`;
+
+  const { company } = await graphqlRequest(query, { id });
+  return company;
+}
+
+export async function createJob(input) {
+  const mutation = `mutation CreateJob($input: CreateJobInput){
+        job: createJob(input: $input) {
+          id
+          title
+          company {
+            id
+            name
+          }
+        }
+      }`;
+  const { job } = await graphqlRequest(mutation, { input });
+  return job;
+}
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,14 +1,31 @@
 const db = require("./db");
 
 const Query = {
+  company: (root, { id }) => db.companies.get(id),
+  job: (root, { id }) => db.jobs.get(id),
   jobs: () => db.jobs.list(),
+};
+
+const Mutation = {
+  createJob: (root, { input }) => {
+    const { companyId, title, description } = input;
+    const id = db.jobs.create({ companyId, title, description });
+    return db.jobs.get(id);
+  },
 };
 
 const Job = {
   company: (job) => db.companies.get(job.companyId),
 };
 
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
+};
+
 module.exports = {
   Query,
   Job,
+  Company,
+  Mutation,
 };
